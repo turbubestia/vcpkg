82e1bbe2f80fff319a998741a49310c486b40d8f
Turbubestia <33103381+turbubestia@users.noreply.github.com>
2026-09-06 00:41:29 -0700

    testing wirhout REF

--- ports/fzy-match/portfile.cmake
+++ ports/fzy-match/portfile.cmake
@@ -2,9 +2,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO turbubestia/fzy-match
-    REF "main"
+    # REF "main"
     # Set to 0 initially so vcpkg calculates the actual SHA512 hash for you
-    HEAD_REF "main"
+    # HEAD_REF "main"
 )
 
 vcpkg_apply_patch(
